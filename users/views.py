fcf7de0fbbc1e760659959b79c8082a948c0a736
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-04 16:28:04 -0400

    update navbar buttons, changes to login/logout

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,3 +8,7 @@ def login(request):
 
 def logout(request):
     return render(request, 'users/logout.html')
+
+def register(request):
+    form = UserCreationForm()
+    return render(request, 'users/register.html', {'form': form})

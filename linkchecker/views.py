8e0adeded26297fffa35a8c8df26282d01310952
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-07 13:01:59 -0400

    update linkchecker

--- linkchecker/views.py
+++ linkchecker/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def linkchecker(request):
+    context = {
+        'page_title': 'Link Checker'
+    }
+    return render(request, 'linkchecker/index.html', context)
--- visualization/views.py
+++ visualization/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def visualization(request):
+    context = {
+        'page_title': 'Data Visualization'
+    }
+    return render(request, 'visualization/index.html', context)

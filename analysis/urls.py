64d3eefc05937e6a7c3ac4a647354d5cabf2feeb
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-07 13:14:44 -0400

    remove about

--- analysis/urls.py
+++ analysis/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='analysis-home'),
-    path('about/', views.about, name='analysis-about'),
     path('download/', views.Download_csv, name='analysis-csv-download'),
 ]

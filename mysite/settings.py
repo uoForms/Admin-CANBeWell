93469727badd54b2e4a1b0e39f1bd8f4afecf727
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-12 15:22:04 -0400

    add static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,7 +125,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = '/home/CanBeWell/Admin-CANBeWell/static'
+#STATIC_ROOT = '/home/CanBeWell/Admin-CANBeWell/static'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # specify the allauth backend
 AUTHENTICATION_BACKENDS = (

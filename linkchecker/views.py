55e071c25ed67bdaee4143dac0724293de8e1d8a
XinmanLi <69054303+XinmanLi@users.noreply.github.com>
2020-10-20 02:23:18 -0400

    Update views.py

--- linkchecker/views.py
+++ linkchecker/views.py
@@ -21,18 +21,17 @@ def output(request):
     for line in lines:
         temp=line.replace('\n','')
         aa.append(temp)
-    print(aa)
     print(f'\033[31m"start testing"\033[0m')
     for a in aa:
         tempUrl = a
         try :
             opener.open(tempUrl)
-            return(f'\033[34m{tempUrl + "  Pass"}\033[0m')
+            print(f'\033[34m{tempUrl + "  Pass"}\033[0m')
         except urllib.error.HTTPError:
-            return(f'\033[33m{tempUrl+ "  HttpError"}\033[0m') #yellow
+            print(f'\033[33m{tempUrl+ "  HttpError"}\033[0m') #yellow
             time.sleep(2)
         except urllib.error.URLError:
-            return(f'\033[35m{tempUrl + "  URLError"}\033[0m') #purple
+            print(f'\033[35m{tempUrl + "  URLError"}\033[0m') #purple
             time.sleep(2)
         time.sleep(0.1)
     

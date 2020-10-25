ee043bf8834726db0b2660da71ca791d68bc8672
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-25 17:04:56 -0400

    add heroku db settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,6 +140,9 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
  )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
 
 SITE_ID = 1
 

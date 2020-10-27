d06678c07f7a099964df9b9f70a16981cea8bee1
Ali Vaseghnia <ali.vaseghnia@gmail.com>
2020-10-26 21:02:47 -0400

    add basic functionality for linkchecker output page

--- linkchecker/views.py
+++ linkchecker/views.py
@@ -12,6 +12,9 @@ def linkchecker(request):
     return render(request, 'linkchecker/index.html', context)
     
 def output(request):
+    context = {
+        'page_title': 'LinkChecker Output'
+    }
     opener = urllib.request.build_opener()
     opener.addheaders = [('User-agent', 'Mozilla/49.0.2')]
     #Folder name of urls
@@ -22,15 +25,19 @@ def output(request):
         temp=line.replace('\n','')
         aa.append(temp)
     print(f'\033[31m"start testing"\033[0m')
+    results = []
     for a in aa:
         tempUrl = a
         try :
             opener.open(tempUrl)
+            results.append(tempUrl + ' >>> PASS')
             print(f'\033[34m{tempUrl + "  Pass"}\033[0m')
         except urllib.error.HTTPError:
+            results.append(tempUrl + ' >>> HttpError')
             print(f'\033[33m{tempUrl+ "  HttpError"}\033[0m') #yellow
             time.sleep(1)
         except urllib.error.URLError:
+            results.append(tempUrl + ' >>> URLError')
             print(f'\033[35m{tempUrl + "  URLError"}\033[0m') #purple
             time.sleep(1)
         time.sleep(0.1)
@@ -38,4 +45,4 @@ def output(request):
     data = output
     print (data)
     # data = data()
-    return render(request,'linkchecker/output.html',{'data':data})
+    return render(request,'linkchecker/output.html', {'data': data, 'results': results})

78158446730f061a46070e4a16dd2dc70944ecb1
krishna1768 <kbach021@uottawa.ca>
2020-11-08 16:30:39 -0500

    Fixed error with downloading data

--- analysis/urls.py
+++ analysis/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='analysis-home'),
     path('analysis/', views.data, name='analysis-data'),
-    path('download/', views.Download_csv, name='analysis-csv-download'),
+    path('analysis/download/', views.Download_csv, name='analysis-csv-download'),
 ]

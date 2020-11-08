b22f9bd18ff408afe7a6f6da14264419d5dc54e1
XinmanLi <69054303+XinmanLi@users.noreply.github.com>
2020-11-08 03:56:29 -0500

    Update urls.py

--- linkchecker/urls.py
+++ linkchecker/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
     path('', views.linkchecker, name='link-checker'),
-    path('output/', views.output, name="output")
+    path('output/', views.output, name="output"),
+    url('^output/(?P<idx>\d+)$', views.output_item),
 ]

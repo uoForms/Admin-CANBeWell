90bfae18c4023b354392f48d8a94d3da86c22c01
XinmanLi <69054303+XinmanLi@users.noreply.github.com>
2021-02-24 01:32:31 +0800

    Update urls.py

--- linkchecker/urls.py
+++ linkchecker/urls.py
@@ -1,9 +1,8 @@
-from django.urls import include, path
-from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.linkchecker, name='link-checker'),
-    path('output/', views.output, name="output"),
-    url('^output/(?P<idx>\d+)$', views.output_item),
+    path('', views.home, name='analysis-home'),
+    path('analysis/', views.data, name='analysis-data'),
+    path('analysis/download/', views.Download_csv, name='analysis-csv-download'),
 ]

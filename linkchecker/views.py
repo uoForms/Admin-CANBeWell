ca4433fcd516fb0c84553f6700b8bd0eec98e2f6
XinmanLi <69054303+XinmanLi@users.noreply.github.com>
2021-02-24 01:40:56 +0800

    Update views.py

--- linkchecker/views.py
+++ linkchecker/views.py
@@ -1,83 +1,145 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-import re
-import urllib.request
-import time
-import json
-from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.http import HttpResponse,HttpResponseRedirect
+from django.http import Http404
+from django.contrib import messages
+from firebase import firebase
+import pandas as pd
+from .myform import dateRangeForm
+from django.utils.safestring import mark_safe
+import datetime
+import pyrebase
+import csv,io
 
-# Don't change the key of files, the name and path could be changed.
-files = {"en_topic":"linkchecker/Topic-EN.json",
-         "fr_topic":"linkchecker/Topic-FR.json",
-         "en_test":"linkchecker/Test-EN.json",
-         "fr_test":"linkchecker/Test-FR.json"}
 
-# Don't change the key of fileType, the name and path could be changed.
-fileType = {"en_topic":"Topic-EN",
-         "fr_topic":"Topic-FR",
-         "en_test":"Test-EN",
-         "fr_test":"Test-FR"}
+firebaseConfig = {
+    'apiKey': "AIzaSyAPuIZi-ns_KRkpTjpnEbTnnAYGflqwbwI",
+    'authDomain': "canbewell-uottawa.firebaseapp.com",
+    'databaseURL': "https://canbewell-uottawa.firebaseio.com",
+    'projectId': "canbewell-uottawa",
+    'storageBucket': "canbewell-uottawa.appspot.com",
+    'messagingSenderId': "813615648464",
+    'appId': "1:813615648464:web:14052cf90420114be318d5",
+    'measurementId': "G-935F1NQDM2",
+}
+firebaseInit = pyrebase.initialize_app(firebaseConfig)
+auth = firebaseInit.auth()
 
-def linkchecker(request):
+def home(request):
     context = {
-        'page_title': 'LinkChecker'
+        'page_title': 'Home'
     }
-    for key in files:
-        urls = resolveUrlInFile(key)
-        context.update({key:len(urls)})
-    return render(request, 'linkchecker/index.html', context)
+    return render(request, 'analysis/home.html', context)
 
 
-def output(request, fileKey):
-    urls = resolveUrlInFile(fileKey)
-    print(f'\033[31m"start testing"\033[0m')
-    # data = data()
-    return render(request, 'linkchecker/output.html', {'total': len(urls), 'urls':urls, 'fileType': fileType[fileKey]})
+def firebase_date_range(startDate, endDate):
+    diff = endDate - startDate
+    date_list = list()
+    for i in range(diff.days + 1):
+        date_temp = startDate + datetime.timedelta(i)
+        date_temp = date_temp.strftime("%Y%m%d")
+        date_list.append(date_temp)
+    return date_list
 
-def output_item(request,fileKey, idx):
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('User-agent', 'Mozilla/49.0.2')]
-    urls = resolveUrlInFile(fileKey)
-    index = int(idx) - 1
-    if index >= len(urls):
-        return JsonResponse({'index':idx, 'result': ''})
 
-    tempUrl = urls[index]
-    try:
-        opener.open(tempUrl)
-        results = 'PASS'
-        print(f'\033[34m{tempUrl + "  Pass"}\033[0m')
-    except urllib.error.HTTPError:
-        results = 'HttpError'
-        print(f'\033[33m{tempUrl + "  HttpError"}\033[0m')  # yellow
-        time.sleep(0.01)
-    except urllib.error.URLError:
-        results = 'URLError'
-        print(f'\033[35m{tempUrl + "  URLError"}\033[0m')  # purple
-        time.sleep(0.01)
-    return JsonResponse({'index':idx, 'result': results})
+def firebase_live_connection(date_list):
+    fbdata = pd.DataFrame()
+    fbobject = firebase.FirebaseApplication("https://canbewell-uottawa.firebaseio.com/", None)
+    fbdata_temp = fbobject.get("", "")
+    for i in range(0, len(date_list)):
+        try:
+            temp = pd.DataFrame.from_dict(fbdata_temp[date_list[i]], orient='index')
+            fbdata = fbdata.append(temp)
+        except:
+            print('date exception caught -- ', str(date_list[i]))
+    return fbdata
 
-# private function
-def  resolveUrlInFile(fileKey):
-    file = open(files[fileKey], mode='r', encoding='UTF-8')
-    js = json.load(file)
-    jsonKey = ''
-    if fileKey == 'en_test' or fileKey == 'fr_test':
-        jsonKey = 'Patient/Provider Text'
-    elif fileKey == 'fr_topic' or fileKey == 'en_topic':
-        jsonKey = 'General Patient Text'
-    else:
-        return []
-    urls = []
-    for i in range(len(js)):
-        txt = js[i][jsonKey]
-        url = re.findall('https://.*]$', txt)
-        for u in url:
-            u = u.replace(']]','')
-            urls .append(u)
 
-        url = re.findall('http://.*]$', txt)
-        for u in url:
-            u = u.replace(']]','')
-            urls.append(u)
-    return urls
+def data_cleaning(fbdata):
+    avg_view_time = fbdata['pageviewtime'].mean()
+    fbdata['pageviewtime'].fillna(avg_view_time, inplace=True)
+    agerange = [''] * len(fbdata)
+    for i in range(0, len(fbdata)):
+        if fbdata.age[i] == 'all ages':
+            agerange[i] = 'all ages'
+        elif '{:0>3}'.format(fbdata.age[i]) <= '049':
+            agerange[i] = 'Young'
+        elif '{:0>3}'.format(fbdata.age[i]) <= '064':
+            agerange[i] = 'Middle'
+        else:
+            agerange[i] = 'Senior'
+    fbdata.insert(0, 'firebase_json_index', fbdata.index)
+    fbdata.insert(1, 'agerange', agerange)
+    fbdata.reset_index(drop=True, inplace=True)
+    fbdata.index = fbdata.index + 1
+    return fbdata
+
+
+def Download_csv(self):
+    global fbdata
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=canbewell_data_export.csv'
+    fbdata.to_csv(path_or_buf=response, index=True)
+    return response
+
+start_date = "yyyy-mm-dd"
+end_date = "yyyy-mm-dd"
+fbdata = []
+
+def data(request):
+    global context
+    global start_date
+    global end_date
+    global fbdata
+    context = {
+        'page_title': 'Data',
+        'form': dateRangeForm(),
+        'start_date': start_date,
+        'end_date': end_date,
+        'fbdata': fbdata
+    }
+    if request.method == 'POST':
+        request_name = request.POST.get('name')
+        print(request_name)
+        if request_name == 'login_form':
+            try:
+                email = request.POST.get('email')
+                password = request.POST.get('password')
+                firebaseUser = auth.sign_in_with_email_and_password(email, password)
+                print(firebaseUser)
+                validation = 1
+                context = {
+                    'page_title': 'Firebase Data',
+                    'form': dateRangeForm(),
+                    'validation': validation,
+                    'email': email,
+                    'password': password,
+                    'firebaseUser': firebaseUser
+                }
+            except:
+                context['validation'] = 0
+            return render(request, 'analysis/data.html', context)
+
+
+        elif request_name == 'connection_form':
+            form = dateRangeForm(request.POST)
+            if form.is_valid():
+                try:
+                    start_date = form.cleaned_data['start_date']
+                    start_date_obj = datetime.datetime.strptime(start_date, '%Y-%m-%d')
+                    end_date = form.cleaned_data['end_date']
+                    end_date_obj = datetime.datetime.strptime(end_date, '%Y-%m-%d')
+                    date_list = firebase_date_range(start_date_obj, end_date_obj)
+                    if date_list:
+                        fbdata = firebase_live_connection(date_list)
+                        fbdata = data_cleaning(fbdata)
+                        print(type(fbdata))
+                        context['fbdata'] = fbdata
+                        context['start_date'] = start_date
+                        context['end_date'] = end_date
+                except:
+                    messages.error(request, mark_safe("Invalid dates.<br/> No data available"))
+            return render(request, 'analysis/data.html', context)
+
+    return render(request, 'analysis/data.html', context)
+

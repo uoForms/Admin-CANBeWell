847baff04f8286688748eb23011a80107ea8c330
arezoop92 <60947865+arezoop92@users.noreply.github.com>
2021-03-05 13:59:42 -0500

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
